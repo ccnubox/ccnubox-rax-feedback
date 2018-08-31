a15fbab65425257dbfd4e925697bd5d3d2339183
zxc0328 <120797887@qq.com>
2018-08-31 22:45:13 +0800

    feat: v2.0.0-beta1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { createElement, Component } from 'rax';
-import View from 'rax-view';
-import Text from 'rax-text';
-import styles from './App.css';
-import TextInput from 'rax-textinput';
-import Touchable from 'rax-touchable';
-import feedbackService from './services/index';
-import Toast from 'universal-toast';
-// const native = require("@weex-module/test");
+import { createElement, Component } from "rax";
+import View from "rax-view";
+import Text from "rax-text";
+import styles from "./App.css";
+import TextInput from "rax-textinput";
+import Touchable from "rax-touchable";
+import feedbackService from "./services/index";
+import Toast from "universal-toast";
+const native = require("@weex-module/test");
 
 class App extends Component {
   constructor(props) {
@@ -14,26 +14,33 @@ class App extends Component {
     this.state = {
       contact: "",
       content: ""
-    }
+    };
   }
   sendFeedback = () => {
-    let data = {
-      "contact": this.state.contact,
-      "feedback": this.state.content
+    if (this.state.contact === "" || this.state.content === "") {
+      alert("请填写完整您的联系方式和反馈信息");
+      return;
     }
+    let data = {
+      contact: this.state.contact,
+      content: this.state.content
+    };
     feedbackService.postFeedback(data).then(res => {
-      Toast.show('反馈成功', Toast.SHORT)
+      Toast.show("反馈成功，感谢您的配合", Toast.SHORT);
+      native.back();
+    }).catch(e => {
+      alert(JSON.stringify(e))
     })
   };
-  onContactChange = () => {
+  onContactChange = (event) => {
     this.setState({
       contact: event.nativeEvent.text
-    })
+    });
   };
-  onContentChange = () => {
+  onContentChange = (event) => {
     this.setState({
       content: event.nativeEvent.text
-    })
+    });
   };
 
   render() {
@@ -49,17 +56,21 @@ class App extends Component {
               ref="input"
               style={[styles.contactInput, styles.fontSize]}
               onChange={this.onContactChange}
+              placeholder="请填写您联系方式（电话/QQ）"
             />
           </View>
           <TextInput
-            placeholder="您的意见"
+            placeholder="请填写您的意见，欢迎加入华师匣子交流群 576225292 反馈您的问题"
             value={this.state.content}
             onChange={this.onContentChange}
             multiline={true}
             ref="input"
             style={[styles.inputContent, styles.fontSize]}
           />
-          <Touchable onPress={this.sendFeedback} style={[styles.submitBox, styles.center]}>
+          <Touchable
+            onPress={this.sendFeedback}
+            style={[styles.submitBox, styles.center]}
+          >
             <Text style={[styles.fontSize, styles.sendText]}>发送</Text>
           </Touchable>
         </View>
---
+++ webpack.config.update.js
@@ -0,0 +1,20 @@
+// webpack.config.update.js
+const path = require('path');
+const webpack = require("webpack");
+const package = require("./package.json");
+
+module.exports = function update(webpackConfig) {
+  webpackConfig.entry["com.muxistudio.feedback"] = [
+    path.resolve(__dirname, "./src/index.js")
+  ];
+  console.log("current env", process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "production") {
+    webpackConfig.plugins[7].options.include = /\.js$/;
+    webpackConfig.plugins.push(
+      new webpack.BannerPlugin({
+        banner: `com.muxistudio.feedback, version ${package.version}, built time: ${Date()}`
+      })
+    );
+  }
+  return webpackConfig;
+};
